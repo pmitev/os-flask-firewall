82c7838f7964199dfce78130420b5de21ffb71f0
pmitev <pmitev@gmail.com>
2021-02-05 13:02:04 +0100

    projects moved in external file

--- flask-server-v01.py
+++ flask-server-v01.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from flask import Flask, make_response, request, send_from_directory
-from flask import jsonify
+from flask import Flask, make_response, request, send_from_directory, abort
 from flask_ipban import IpBan
-import os 
-import subprocess    
+import os, time 
+import subprocess
 import shlex
+import json
 
 ip_ban = IpBan()
 app = Flask(__name__)
@@ -12,52 +12,71 @@ ip_ban.init_app(app)
 # ufw allow 6768/tcp
 
 
-@app.route("/<project>/<region>/<rule>/<token>", methods=["GET"])
-def get_my_ip(project,region,rule,token):
-    #return jsonify({'ip': request.remote_addr}), 200
+@app.route("/<project>/<region>/<rule>/<port>/<token>", methods=["GET"])
+def get_my_ip(project,region,rule,port,token):
     ipaddr= request.remote_addr
-    text= "Your IP address: " + ipaddr + " is wite-listed for 24 hours"
-    if check_project(project,region,rule,token):
+    if check_project_port(project,region,rule,port,token):
         print("OK")
-        source_env(project,region,rule,token)
-        #add_rule(ipaddr,project,region,rule)
-        print(os.environ['OS_REGION_NAME'])
+        source_env(project,region)
+        #add_rule(ipaddr,project,region,rule,port)
+        response= make_response("Your IP address: " + ipaddr + " is wite-listed for 24 hours",200)
     else:
         print("Wrong")
-        text="Wrong"
-    return text, 200
+        response= make_response('Wrong', 404)
+        abort(404)
+
+    response.mimetype = "text/plain"
+    return response
 
 @app.route("/<project>/<region>/<rule>/WhiteListed/<token>", methods=["GET"])
 def get_white(project,region,rule,token):
-    cmd_txt= f'openstack security group rule list --ingress {rule}';    cmd=shlex.split(cmd_txt)
-    print(cmd_txt)
-    proc= subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = proc.communicate()
-    print(stderr)
-    response = make_response(stdout.decode(), 200)
+    if check_project(project,region,rule,token):
+        cmd_txt= f'openstack security group rule list --ingress {rule}';    cmd=shlex.split(cmd_txt)
+        print(cmd_txt)
+        proc= subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = proc.communicate()
+        print(stderr)
+        response = make_response(stdout.decode(), 200)
+    else:
+        print('Wrong')
+        response= make_response('Wrong', 404)
+        abort(404)
+
     response.mimetype = "text/plain"
     return response
 
 
+# =============================================================================================================
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-def add_rule(ip, project, region, rule):
-    cmd_txt=f'openstack security group rule create {rule} --protocol tcp --dst-port 8787:8787 --remote-ip {ip}/32';     cmd=shlex.split(cmd_txt)
+def add_rule(ip, project, region, rule, port):
+    T= time.strftime("%Y-%m-%dT%H:%M:%S")
+
+    with open("ip_witelist.dat", 'a') as fo:
+        fo.write(str(time.time()) + ip +" "+project+" "+region+" "+rule+" "+port+" "+T+"\n")
+    fo.close()
+
+    cmd_txt=f'openstack security group rule create {rule} --protocol tcp --dst-port {port}:{port} --remote-ip {ip}/32';     cmd=shlex.split(cmd_txt)
     print(cmd_txt)
     proc= subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = proc.communicate()
     print(stderr)
+    with open(str(ip)+".rule", 'ab') as fo1:
+        fo1.write( (">>> "+T+"\n").encode())
+        fo1.write( (">>> "+project+" "+region+"\n").encode())
+        fo1.write(stdout)
+        fo1.write(stderr)
+    fo1.close()
 
 def check_project(project,region,rule,token):
-    if (project in PROJECTS.keys()) and (region in PROJECTS[project]['regions']) and (rule in PROJECTS[project]['rules']) and (token == PROJECTS[project]['token']):
-        return True
-    else:
-        return False
+    return (project in PROJECTS.keys()) and (region in PROJECTS[project]['regions']) and (rule in PROJECTS[project]['rules']) and (token == PROJECTS[project]['token'])
 
-def source_env(project,region,rule,token):
+
+def check_project_port(project,region,rule,port,token):
+    return (project in PROJECTS.keys()) and (region in PROJECTS[project]['regions']) and (rule in PROJECTS[project]['rules']) and (port in PROJECTS[project]['ports']) and (token == PROJECTS[project]['token'])
+
+
+def source_env(project,region):
     cmd_txt=f'env -i bash -c "source {project}-{region}-openrc.sh && env"' ; cmd=shlex.split(cmd_txt)
     print(cmd_txt)
     proc= subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -65,11 +84,21 @@ def source_env(project,region,rule,token):
         (key, _, value) = line.decode().partition("=")
         os.environ[key] = value.rstrip()
     proc.communicate()
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
     return True
 
 if __name__ == '__main__':
-    PROJECTS={'SNIC_2020_20-57': {'regions': ['east-1', 'C3SE'],
-      'token': '97df913f-6789-4d74-8617-5afad9e5ccc6',
-      'rules': ['Rstudio'] }}
+
+#    PROJECTS={'SNIC_2020_20-57': {'regions': ['east-1', 'C3SE'],
+#      'token': '97df913f-6789-4d74-8617-5afad9e5ccc6',
+#      'rules': ['Rstudio'],
+#      'ports': ['8787'] }
+#      }
+
+    with open('projects.json','r') as f:
+        PROJECTS=json.load(f)
 
     app.run(host="0.0.0.0", port=5000, debug=True)
